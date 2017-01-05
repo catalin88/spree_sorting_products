8c596498745800b27eee74f46173b10f523e81a0
catalin88 <catalin_t_vulcan@yahoo.com>
2017-01-05 03:27:37 +0100

    Add sorting to index products

---
+++ app/controllers/spree/home_controller_decorator.rb
@@ -0,0 +1,10 @@
+Spree::HomeController.class_eval do
+  include SortingParams
+
+  alias_method :old_index, :index
+
+  def index
+    old_index # Like calling super: http://stackoverflow.com/a/13806783/73673
+    @products = @products.select('spree_products.*, spree_prices.amount').send(sorting_scope)
+  end
+end
